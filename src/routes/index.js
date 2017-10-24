ed394c01f5980e05b09367e3673ca168213e525b
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-10-24 12:09:59 -0400

    Todo comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,12 @@ router.get('/', function (req, res) {
     res.render('index')
   }); 
 
+
+// TODO: Add a POST route to create new bill entries
+
+// TODO: Add a PUT route to update existing bill entries 
+
+// TODO: Add a DELETE route to delete bill entries
+
+
 module.exports = router;
