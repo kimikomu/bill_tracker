7d610074a99b555a5ddeeb8a74b3a6c1fec4e5f2
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-10-27 16:35:55 -0400

    Add data service to import data from json file

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,20 +1,28 @@
 angular.module('billTrackerApp', [])
-    // set up the custom directive
-    .controller('MainCtlr', function($scope) {
-        $scope.stopEditing = function() {
-            // TODO: if user is not editing an input field (input type != text), "editing" = false
-            
-            console.log("blur test");
-        };
 
-        // test array
-        $scope.bills = [
-            {"name": "water", "amount": "137", "due": "Nov 14"},
-            {"name": "car", "amount": "268", "due": "Nov 21"},
-            {"name": "car insurance", "amount": "123", "due": "Nov 18"},
-            {"name": "Verizon", "amount": "120", "due": "Nov 6"},
-            {"name": "trash", "amount": "20", "due": "Nov 31"},
-        ]
+// set up the custom directive
+.controller('MainCtlr', function($scope, dataService) {   
+    $scope.stopEditing = function() {
+        // TODO: if user is not editing an input field (input type != text), "editing" = false
+        console.log("blur test");
+    };
+
+    $scope.helloConsole = dataService.helloConsole;
+
+    dataService.getBills(function(res) {
+        console.log(res.data);
+        $scope.bills = res.data;
+    });
+})
+.service('dataService', function($http) {
+    this.helloConsole = function() {
+        console.log("Testing helloConsole");
+    };
+
+    this.getBills = function(callback) {
+        $http.get('data/bills.json')
+        .then(callback)
+    }
 });
 
 
