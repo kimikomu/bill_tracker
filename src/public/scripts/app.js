495e44b070459e453a65645201eb0feb4d8d9b82
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-10-27 12:27:29 -0400

    Add array for test content. Set up app to repeat bill content

--- src/public/scripts/app.js
+++ src/public/scripts/app.js
@@ -1,6 +1,18 @@
 angular.module('billTrackerApp', [])
+    // set up the custom directive
     .controller('MainCtlr', function($scope) {
         $scope.logTest = function() {
-            console.log("Testing logTest");
+            console.log("Bill has been payed!");
         };
-    });
+
+        // test array
+        $scope.bills = [
+            {"name": "water", "amount": "137"},
+            {"name": "car", "amount": "268"},
+            {"name": "car insurance", "amount": "123"},
+            {"name": "Verizon", "amount": "120"},
+            {"name": "trash", "amount": "20"},
+        ]
+});
+
+
