639340fe26c762038e95f9f493484daa842528eb
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-10-31 13:31:29 -0400

    Add put route

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,7 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const Bill = require('../models/bill.model.js');
 
-// Read
+// GET
 router.get('/bills', function (req, res, next) {
   Bill.find({}, function(err, bills) {
     if (err) {
@@ -17,7 +17,7 @@ router.get('/bills', function (req, res, next) {
   });
 }); 
 
-// Create
+// POST
 router.post('/bills', function(req, res, next) {
   const bill = req.body;
   Bill.create(bill, function(err, bill) {
@@ -29,7 +29,22 @@ router.post('/bills', function(req, res, next) {
   });
 });
 
-// TODO: Add a PUT route to update existing bill entries 
+// PUT
+router.put('/bills/:id', function(req, res, next) {
+  const id = req.params.id;
+  const bill = req.body;
+  if(bill && bill._id !== id) {
+    return res.status(500).json({err: "Ids do not match"});
+  }
+  Bill.findByIdAndUpdate(id, bill, {new: true}, function(err, bill) {
+    if(err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+    res.json({'bill': bill, message: 'Bill Updated'});
+  });
+});
+
 
 // TODO: Add a DELETE route to delete bill entries
 
