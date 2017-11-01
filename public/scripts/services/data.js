06195cd1e3e67444c05647e295249d421512731b
Kimiko Muwanguzi <kimikomu@gmail.com>
2017-11-01 19:13:52 -0400

    Enable update functionality

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -19,11 +19,19 @@ angular.module('billTrackerApp')
     // save a bill
     this.saveBill = function(bill) {
         let request;
+        // create new bill
         if(!bill._id) {
             request = $http.post('/routes/bills', bill);
+        // update preexisting bill
+        } else {
+            request = $http.put(`/routes/bills/${bill._id}`, bill).then(function(result) {
+                bill = result.data.bill;
+                return bill;
+            });
         };
         bill.edited = false;
-        return $q.resolve(this.bill).then(function(results) {
+        
+        return $q.resolve(this.bill).then(function(result) {
             console.log(`Saved the ${bill.name} bill`);
         });
     };
@@ -34,17 +42,23 @@ angular.module('billTrackerApp')
         const queue = [];
         bills.forEach(function(bill) {
             let request;
-            // new bills get posted to the route
+            // create new bills
             if(!bill._id) {
                 request = $http.post('/routes/bills', bill);
+            // update preexisting bills
+            } else {
+                request = $http.put(`/routes/bills/${bill._id}`, bill).then(function(result) {
+                    bill = result.data.bill;
+                    return bill;
+                });
             };
-            
             bill.edited = false;
+
             queue.push(request);
             console.log(`${bill.name} was edited`);
         });
         // return all bills in the array
-        return $q.all(queue).then(function(results) {
+        return $q.all(queue).then(function(result) {
             console.log(`Saved ${bills.length} bills`);            
         })
     };
